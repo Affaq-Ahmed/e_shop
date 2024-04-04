f4c19f002e9829f38a234f02029708d0ff3e3c4f
Affaq-Ahmed <aaawan165@gmail.com>
2024-04-04 12:23:59 +0500

    Add Banner schema to sanity/schema.ts

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity';
 import Product from './schemas/product';
+import Banner from './schemas/banner';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-	types: [Product],
+	types: [Product, Banner],
 };
