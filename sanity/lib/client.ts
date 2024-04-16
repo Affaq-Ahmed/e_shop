3c9d610b6865c33c0f27d0bd7ee7c95fb02b25c3
Affaq-Ahmed <aaawan165@gmail.com>
2024-04-16 21:22:55 +0500

    add Image loader for nextjs

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,11 +1,22 @@
-import { createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
 
-import { apiVersion, dataset, projectId, useCdn, token } from '../env'
+import {
+	apiVersion,
+	dataset,
+	projectId,
+	useCdn,
+	token,
+} from '../env';
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-  token
-})
+	apiVersion,
+	dataset,
+	projectId,
+	useCdn,
+	token,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source: any) => builder.image(source)
