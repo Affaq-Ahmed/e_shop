a3fe0fc01c222b1b3f7f91788af75cd0bc2164a7
Affaq-Ahmed <aaawan165@gmail.com>
2024-04-16 13:30:52 +0500

    feat: sanityChanges

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,7 +11,13 @@ export const projectId = assertValue(
 	'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 );
 
-export const useCdn = false;
+export const useCdn = true;
+
+export const token = assertValue(
+	process.env.NEXT_PUBLIC_SANITY_TOKEN ||
+		'skahzlNGC0BYwTa2AOZAq9klLnrJY7ZAU0CxLc9C5XaCM4jOV9SRZSQ1mHcmNMunrdIKz6RYzGSZZc94JLOjvL9iRwqpErDZdXRI3OVtS7bkSKbIWj5zrmlZzx96Talwp1T9jtRfZUVkXQEiuojWLUM2T5vzRzTyAPLt1H78bqL7ywrdX6kj',
+	'Missing environment variable: NEXT_PUBLIC_SANITY_TOKEN'
+);
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
 	if (v === undefined) {
